0fbf9b43be297be42610e41973931d5d025ef0d3
kikecruzmamani <http://github.com/kikecruzmamani/ejemplojs.gitgit config --global user.name kikecruzmamanigit config --global user.email http://github.com/kikecruzmamani/ejemplojs.git>
2016-10-29 09:37:38 -0400

    iniciando repo

---
+++ ejemplo1.js
@@ -0,0 +1,3 @@
+//Calcule el valor de 8 dolares en bolivianos
+var x=8;
+console.log(x+"dolares son:" +x * 6.96 +"bolivianos");
---
+++ ejemplo5.js
@@ -0,0 +1,12 @@
+//console.log(3+7);//20
+//console.log("Resta de dos numeros 13-7 : " );//6
+//console.log(13-7);//6
+//console.log((8*2-4)/3);//4
+//console.log("ejecucion se interrumpe:");//Error_de_ejecucion
+console.log("\"hola\"");
+console.log("suma de dos numeros 13+7 : " );//6
+console.log("Resta de dos numeros +13 : " );//6
+console.log(+13);
+console.log("concatenacion de strings \"Hola\" + \"Perez\":");//hola pepe
+console.log("Hola"   +   "Perez");
+ 
